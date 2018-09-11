63cc9c788a5f6a6dd7b6b7bcbc045199858bbcd8
Wera Chodanionek <werachodanionek@gmail.com>
2018-09-11 14:12:15 +0200

    fixes in script

--- script.js
+++ script.js
@@ -15,49 +15,10 @@ $(document).ready(function() {
 
 });
 
-$( function () {
-    var scrolled;
-    var $window = $(window);
-    var el
-    $window.on('scroll', function () {
-        scrolled = $(window).scrollTop();
-        
-        el = $('.parallax');
-        el.each(function () {
-            var parallax = scrolled * $(this).data('speed');
-            $(this).css({
-                'top': parallax
-            });
-        });
-    });
-});
-
-
-jQuery(document).ready(function() {
-    $(".square_miniature").mouseover(function() {
-        $(this).css("opacity", "1");
-    }).mouseout(function() {
-        $(this).css("opacity", "0.2");
-    });
-});
-
-//pojawiani się tekst
-$(document).ready(function(){
-
-    $('*[data-animate]').addClass('hide').each(function(){
-  $(this).viewportChecker({
-    classToAdd: 'show animated ' + $(this).data('animate'),
-    classToRemove: 'hide',
-    offset: '30%'
-  });
-});
-
-//pojawienie się linka po czasie
-$(document).ready(function() { 
-    setTimeout(function() { 
-        $('.link').fadeIn(); 
- }, 3000); 
-});
+$(document).ready(function() {
+    $('.fontello_html').delay(1000).fadeIn(1000);
+    $('.fontello_css').delay(1500).fadeIn(1000);
+    $('.fontello_git').delay(2000).fadeIn(1000);
+    $('.fontello_wordpress').delay(2500).fadeIn(1000);
     
-
 });
